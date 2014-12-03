00b01937a530c5e60c671cbe2f8f3f5834ac01cb
Ville Ingman <ville@vaadin.com>
2014-12-03 13:43:51 +0200

    Simplified data reading.

--- backend/src/main/java/com/drone/Main.java
+++ backend/src/main/java/com/drone/Main.java
@@ -1,33 +1,44 @@
 package com.drone;
 
-import org.springframework.context.ApplicationContext;
+import java.net.UnknownHostException;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
 
-import java.net.UnknownHostException;
-
 public class Main {
 
-    @Configuration
-    public static class DroneClientConfiguration {
+	static AnnotationConfigApplicationContext applicationContext = null;
 
-        @Bean
-        TaskExecutor taskExecutor (){
-            return new SimpleAsyncTaskExecutor();
-        }
+	@Configuration
+	public static class DroneClientConfiguration {
 
-        @Bean
-        DroneTemplate provideDroneTemplate(TaskExecutor taskExecutor ) throws UnknownHostException {
-            DroneStateChangeCallback listener = latestState -> System.out.println("the latest state is: " + latestState);
-            return new DroneTemplate (  taskExecutor );
-        }
-    }
+		@Bean
+		TaskExecutor taskExecutor() {
+			return new SimpleAsyncTaskExecutor();
+		}
 
-    public static void main(String[] args) throws Throwable {
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(DroneClientConfiguration.class);
-        applicationContext.start();
-    }
+		@Bean
+		DroneTemplate provideDroneTemplate(TaskExecutor taskExecutor)
+				throws UnknownHostException {
+			DroneStateChangeCallback listener = latestState -> {
+				System.out.println("altitude: " + latestState.getAltitude());
+			};
+			return new DroneTemplate(taskExecutor, listener);
+		}
+	}
+
+	public static void main(String[] args) throws Throwable {
+		applicationContext = new AnnotationConfigApplicationContext(
+				DroneClientConfiguration.class);
+		applicationContext.start();
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		super.finalize();
+		applicationContext.close();
+	}
 }
